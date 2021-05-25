18cc839f97512b8a69c5c639accbc00f5601dc69
Maya <melokitty.1003@gmail.com>
2021-05-25 17:35:59 +0900

    add paintscreen

---
+++ lib/screens/PaintScreen.dart
@@ -0,0 +1,54 @@
+import 'package:drawing_app/define/Common.dart';
+import 'package:flutter/material.dart';
+
+class PaintScreen extends StatefulWidget {
+  @override
+  _PaintScreenState createState() => _PaintScreenState();
+}
+
+class _PaintScreenState extends State<PaintScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('お絵描き'),
+      ),
+      body: Container(
+        color: Common.primaryColor.shade50
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          FloatingActionButton(
+            heroTag: 'undo',
+            onPressed: () {
+
+            },
+            child: Text('Undo'),
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          FloatingActionButton(
+            heroTag: 'redo',
+            onPressed: () {
+
+            },
+            child: Text('Redo'),
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          FloatingActionButton(
+            heroTag: 'clear',
+            onPressed: () {
+
+            },
+            child: Text('Clear'),
+          ),
+        ],
+      ),
+    );
+  }
+}
